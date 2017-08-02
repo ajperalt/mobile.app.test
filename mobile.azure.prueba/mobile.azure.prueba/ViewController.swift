f895460ffc0930126347197723fe43e09bf239f3
Alvaro Jose Peralta <aperalta@lucasian.com>
2017-08-01 20:00:09 -0400

    add example

---
+++ mobile.azure.prueba/mobile.azure.prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobile.azure.prueba
+//
+//  Created by Lab4u on 01-08-17.
+//  Copyright © 2017 Lab4u. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ mobile.azure.prueba/mobile.azure.pruebaTests/mobile_azure_pruebaTests.swift
@@ -0,0 +1,36 @@
+//
+//  mobile_azure_pruebaTests.swift
+//  mobile.azure.pruebaTests
+//
+//  Created by Lab4u on 01-08-17.
+//  Copyright © 2017 Lab4u. All rights reserved.
+//
+
+import XCTest
+@testable import mobile_azure_prueba
+
+class mobile_azure_pruebaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
